2dcac62e7f45ddf363c1b13c7d866afc66970875
OrkVasya <OrkVasya@gmail.com>
2023-03-05 19:55:22 +0300

    1.5. Поля объекта

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,7 +1,33 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String food;
+
+    private String name;
+
+    public void getNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.getNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.getNick("Black");
+        black.eat("fish");
+        black.show();
     }
 }
