7fac1a0a224edfa75971fe1ba47b1ddf86144a77
OrkVasya <OrkVasya@gmail.com>
2023-04-18 22:42:30 +0300

    4.2. Интерфейс Input

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void passengers(int passengerNum) {
+
+    }
+
+    @Override
+    public double refuel(int amount) {
+        return 0;
+    }
+}
