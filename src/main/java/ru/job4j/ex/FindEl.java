5200e06c3f93983e979c9d01a9a2b9c38b80bf4d
OrkVasya <OrkVasya@gmail.com>
2023-07-01 22:16:17 +0300

    0.4. Пользовательские исключения. [#219367  [#219367]]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException();
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            String[] strs = {"a", "b", "c", "d"};
+            System.out.println(indexOf(strs, "b"));
+            System.out.println(indexOf(strs, "z"));
+        } catch (ElementNotFoundException e) {
+            System.out.println("Element not found");
+        }
+    }
+}
