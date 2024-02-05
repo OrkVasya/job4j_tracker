d1ff2bfe9641e7fbca1c1dd8ba7df458a3a20dac
OrkVasya <OrkVasya@gmail.com>
2024-02-05 19:15:34 +0300

    4. Телефонный справочник на базе ArrayList [#41598 #479173]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -17,13 +17,8 @@ public class PhoneDictionary {
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getName().contains(key)) {
-                result.add(person);
-            } else if (person.getSurname().contains(key)) {
-                result.add(person);
-            } else if (person.getPhone().contains(key)) {
-                result.add(person);
-            } else if (person.getAddress().contains(key)) {
+            if (person.getName().contains(key) || person.getSurname().contains(key)
+                    || person.getPhone().contains(key) || person.getAddress().contains(key)) {
                 result.add(person);
             }
         }
