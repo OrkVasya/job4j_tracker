fa4071aefa335e5e4289ec42b5662c7252f4f605
OrkVasya <OrkVasya@gmail.com>
2024-03-25 16:53:29 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("1@email.ru", "1");
+        map.put("2@email.ru", "2");
+        map.put("3@email.ru", "3");
+        map.put("4@email.ru", "4");
+        for (var entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+    }
+}
